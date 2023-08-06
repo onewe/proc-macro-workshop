9ed0ca99839d071b6c4dfb363c0fcfe148816200
onewe <admin@onew.me>
2023-08-06 22:50:39 +0800

    finish  lesson bitfield 01

--- bitfield/src/lib.rs
+++ bitfield/src/lib.rs
@@ -11,5 +11,34 @@
 // From the perspective of a user of this crate, they get all the necessary APIs
 // (macro, trait, struct) through the one bitfield crate.
 pub use bitfield_impl::bitfield;
+use paste::paste;
 
 // TODO other things
+
+pub trait Specifier {
+    const BITS: u16;
+}
+
+
+macro_rules!  gen_b_type{
+    ($($id:literal),*) => {
+        paste! {
+            $(
+                pub struct [<B $id>] {
+
+                }
+
+                impl $crate::Specifier for [<B $id>] {
+                    const BITS: u16 = $id;
+                }
+            )*
+           
+        }
+    };
+}
+
+gen_b_type![
+    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,
+    31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,
+    58,59,60,61,62,63,64
+];
